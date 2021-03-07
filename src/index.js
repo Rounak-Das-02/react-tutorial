36cdc8944057aa68752b5846d05431af22016639
Rounak-Das-02 <dasrounak2002@gmail.com>
2021-03-07 12:16:09 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ReactDOM from 'react-dom';
 import {books} from './books.js'
 import Book from './Book.js'
 
 
+
 const BookList = () =>  {
 
+const [value , setValue] = useState(window.innerWidth);
+const checkSize = () => setValue(window.innerWidth);
+
+useEffect(()=>{
+            window.addEventListener('resize' , checkSize)
+},[value]);
+console.log("Render Component");
 
 
-    return (
-        <section className="BookList">
-            {books.map((book) => {
-                return (
-                    <Book key = {book.id} {...book}></Book>
-                )
-            })}
-        </section>
-    )
+
+    return(
+        <div>
+            <h1>Window : {value}px</h1>
+            <span>
+                <button onClick={()=>{setValue(value+1)}}>Update</button>
+                <button onClick={()=>{setValue(0)}}>Reset</button>
+            </span>
+            </div>
+    );
+
+
+
+
+
+
+    // return (
+    //     <section className="BookList">
+    //         {books.map((book) => {
+    //             return (
+    //                 <Book key = {book.id} {...book}></Book>
+    //             )
+    //         })}
+    //     </section>
+    // )
 } 
 
 ReactDOM.render(<BookList/> , document.getElementById('root'));
